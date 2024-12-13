00eba81bf4c0b3972abfa2be5b2a04d2598affb1
limecaster <bang.dovan38@gmail.com>
2024-12-13 18:59:00 +0700

    first commit

--- controller/search.controller.ts
+++ controller/search.controller.ts
@@ -1,12 +1,18 @@
 import { Body } from "@nestjs/common";
 import { Controller, Get, Param, Query } from "@nestjs/common";
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { SearchProductService } from "service/search-product.service";
 
+@ApiTags('search')
 @Controller('search')
 export class SearchController {
   constructor(private readonly searchProductService: SearchProductService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Full-text search for products by name' })
+  @ApiBody({ description: 'Name of the product to search for' })
+  @ApiResponse({ status: 200, description: 'List of products found' })
+  // curl -X GET http://127.0.0.1:3001/search -H "Content-Type: application/json" -d '{"name": "AMD Ryzen 7"}'
   async searchProduct(@Body('name') name: string) {
     return (await this.searchProductService.searchProduct(name)).records.map((record) => record.get('node').properties);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const openApiConfig = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API Documentation for the project')
+    .setVersion('1.0')
+    .addTag('neo4j')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, openApiConfig);
+  SwaggerModule.setup('api', app, document);
+
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
