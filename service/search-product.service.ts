2ed8bbe02c2fe4dc8351368620e796ae4554aad5
limecaster <bang.dovan38@gmail.com>
2024-12-21 00:09:49 +0700

    add search product

--- service/search-product.service.ts
+++ service/search-product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Neo4jConfigService } from 'config/neo4j.config';
+import { off } from 'process';
 
 @Injectable()
 export class SearchProductService {
@@ -14,19 +15,53 @@ export class SearchProductService {
     }
   }
 
-  async searchProduct(name: string) {
-    // Do a full-text search for a product by name
-    // Requires a FULLTEXT index on the each product node's name property
-    
-    const query = `
-      CALL db.index.fulltext.queryNodes('searchProductName', $name)
-      YIELD node, score
-      RETURN node, score
-      LIMIT 100;
-    `;
+  async searchProduct(name: string, page: number = 1, limit: number = 10) {
     if (!name) {
-        throw new Error('Name is required');
+      throw new Error('Name is required');
     }
-    return this.runQuery(query, { name });
+
+    // Ensure page and limit are integers
+    const pageNum = Math.max(1, Math.floor(page)); // Ensure page is at least 1
+    const limitNum = Math.max(1, Math.floor(limit)); // Ensure limit is at least 1
+
+    const offset = (pageNum - 1) * limitNum;
+
+    const query = `
+        CALL db.index.fulltext.queryNodes('searchProductName', $name)
+        YIELD node, score
+        RETURN node, score
+        SKIP toInteger($offset)
+        LIMIT toInteger($limit);
+    `;
+
+    const result = await this.runQuery(query, {
+      name,
+      offset,
+      limit: limitNum,
+    });
+    
+    const countTotalRecords = `
+      CALL db.index.fulltext.queryNodes('searchProductName', $name)
+      YIELD node
+      RETURN count(node) as totalRecords;
+    `;
+
+    const countResult = await this.runQuery(countTotalRecords, { name });
+    const countTotalRecordsResult = countResult.records[0].get('totalRecords').toNumber();
+
+    return {
+      results: result.records.map((record) => {
+        const node = record.get('node');
+        return {
+          id: node.properties.id,
+          name: node.properties.name,
+          price: node.properties.price || null,
+          imageUrl: node.properties.imageUrl || null,
+          score: record.get('score'),
+        };
+      }),
+      currentPage: pageNum,
+      totalPages: Math.ceil(countTotalRecordsResult / limitNum)
+    };
   }
 }
